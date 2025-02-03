1aeb6fa1897face5257ee854e8bcf7e96a4992d0
POI <dileeppuli3@gmail.com>
2025-02-03 23:19:09 +0530

    sorting algorithm

---
+++ Algortigms/src/BUbbleSort.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class BUbbleSort {
+
+    public static void main(String[] args) {
+        int[] arr = { 64, 34, 25, 12, 22, 11, 90 };
+        int n = arr.length;
+        bubbleSort(arr, n);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void bubbleSort(int[] arr, int n){
+        int i, j, temp;
+        boolean swapped;
+        for (i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+
+                    // Swap arr[j] and arr[j+1]
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            if (!swapped)
+                break;
+        }
+    }
+}

0716ea0f139c68c0e8375d0f66d6c7edbbc5004b
POI <dileeppuli3@gmail.com>
2025-02-27 23:04:11 +0530

    String group

--- Java8/src/StringGroup.java
+++ Java8/src/StringGroup.java
@@ -6,11 +6,16 @@ import java.util.stream.Collectors;
 public class StringGroup {
 
     public static void main(String[] args) {
+
+        String hi = "hi";
+        String he = new String("hi");
         List<String> list = Arrays.asList("Cat", "Dog", "Sky", "Sun", "Star", "Apple", "Bread");
 
         Map<Integer, Long> collect = list.stream().collect(Collectors.groupingBy(String::length, Collectors.counting()));
 
+        Map<Integer, List<String>> collect1 = list.stream().collect(Collectors.groupingBy(String::length));
 
         System.out.println(collect);
+        System.out.println(collect1);
     }
 }
